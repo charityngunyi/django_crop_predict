0c84411f5d9b52a2ec69668cf7b1ca7d33a97603
Charity Ngunyi <ngunyicharity@gmail.com>
2022-06-30 13:20:50 +0300

    changed csv files to point to github repository

--- PredictionSystem/ModelAnalysis/kneighbour_predict.py
+++ PredictionSystem/ModelAnalysis/kneighbour_predict.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-data = pd.read_csv('/home/charity/Django2.1/CropPredictionSystem/PredictionSystem/crops/crop_df.csv')
+data = pd.read_csv('https://github.com/charityngunyi/django_crop_predict/blob/main/PredictionSystem/crops/crop_df.csv')
 
 X = data.drop(['Id', 'type'], axis=1)
 y = data['type']
--- PredictionSystem/ModelAnalysis/model_analysis.py
+++ PredictionSystem/ModelAnalysis/model_analysis.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-data = pd.read_csv('/home/charity/Django2.1/CropPredictionSystem/PredictionSystem/crops/crop_df.csv')
+data = pd.read_csv('https://github.com/charityngunyi/django_crop_predict/blob/main/PredictionSystem/crops/crop_df.csv')
 
 X = data.drop(['Id', 'type'], axis=1)
 y = data['type']
