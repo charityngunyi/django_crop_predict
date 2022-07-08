f143949e79d5b4863f3a8505fe19a5fc387d9bd0
Charity Ngunyi <ngunyicharity@gmail.com>
2022-07-08 15:37:35 +0300

    production settings

--- InformationSystem/views.py
+++ InformationSystem/views.py
@@ -30,5 +30,5 @@ def about(request):
 def crops_information_view(request):
     return render(request, 'information/information_system.html')
 def soil_sampling(request):
-    data = open("/home/charity/Django2.1/CropPredictionSystem/templates/xml/soilsampling.xml")
+    data = open("https://raw.githubusercontent.com/charityngunyi/django_crop_predict/main/templates/xml/soilsampling.xml")
     return HttpResponse(data, content_type="application/xml")
--- PredictionSystem/ModelAnalysis/model_analysis.py
+++ PredictionSystem/ModelAnalysis/model_analysis.py
@@ -7,9 +7,13 @@ import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 
-# remote_filepath = 'https://raw.githubusercontent.com/charityngunyi/django_crop_predict/main/PredictionSystem/crops/crop_df.csv'
-local_filepath = '/home/charity/Django2.1/CropPredictionSystem/PredictionSystem/crops/crop_df.csv'
-data = pd.read_csv(local_filepath)
+# Access the dataset
+remote_filepath = 'https://raw.githubusercontent.com/charityngunyi/django_crop_predict/main/PredictionSystem/crops/crop_df.csv'
+# local_filepath = '/home/charity/Django2.1/CropPredictionSystem/PredictionSystem/crops/crop_df.csv'
+# For development
+# data = pd.read_csv(local_filepath)
+# for production
+data = pd.read_csv(remote_filepath)
 
 X = data.drop(['Id', 'type'], axis=1)
 y = data['type']
